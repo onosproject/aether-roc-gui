8a073da8be57f1b053ecf0ef92f3350679ee8aaa
James Keohane <james@opennetworking.org>
2021-02-10 18:03:23 +0000

    Adding new data type checks

--- src/app/basket.service.ts
+++ src/app/basket.service.ts
@@ -3,49 +3,48 @@
  *
  * SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
  */
-import { Injectable } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {Injectable} from '@angular/core';
+import {AbstractControl, FormArray, FormControl, FormGroup} from '@angular/forms';
 import {AETHER_TARGETS} from '../environments/environment';
+import {ArrayType} from '@angular/compiler';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class BasketService {
 
-    constructor() { }
+    constructor() {
+    }
 
-    logKeyValuePairs(group: FormGroup, parent?: string): void {
+    logKeyValuePairs(abstractControl: AbstractControl, parent?: string): void {
 
         // Path is either '/' if undefined == true or '/' + parent if false
         const path = (parent === undefined) ? '/' : '/' + parent;
-        Object.keys(group.controls).forEach((key: string) => {
 
-            // Get a reference to the control using the FormGroup.get() method
-            const abstractControl = group.get(key);
-
-            // If the control is an instance of FormGroup i.e a nested FormGroup
-            // then recursively call this same method (logKeyValuePairs) passing it
-            // the FormGroup so we can get to the form controls in it
-
-            if (abstractControl instanceof FormGroup) {
-                this.logKeyValuePairs(abstractControl, path === '/' ? key : parent + '/' + key);
-                // If the control is not a FormGroup then we know it's a FormControl
-            } else {
-                if (abstractControl.pristine === false && abstractControl.touched === true) {
-                    if (abstractControl.value === '') {
-                        const fullPath = '/basket-delete' + path + '/' + key;
-                        localStorage.setItem(fullPath, abstractControl.value);
-                        console.log('Changed PATH: ' + fullPath + ' && Value = ' + abstractControl.value);
-                    } else {
-                        const fullPath = '/basket-update' + path + '/' + key;
-                        localStorage.setItem(fullPath, abstractControl.value);
-                        console.log('Changed PATH: ' + fullPath + ' && Value = ' + abstractControl.value);
-                    }
+        if (abstractControl instanceof FormGroup) {
+            Object.keys(abstractControl.controls).forEach((key: string) => {
+                this.logKeyValuePairs(abstractControl.controls[key], path === '/' ? key : parent + '/' + key);
+            });
+            // If the control is not a FormGroup then we know it's a FormControl
+        } else if (abstractControl instanceof FormArray) {
+            (abstractControl as FormArray).controls.forEach((item, idx) => {
+                this.logKeyValuePairs(item, path === '/' ? 'i' + idx : parent + '/' + String(idx));
+            });
+        } else {
+            if (abstractControl.pristine === false && abstractControl.touched === true) {
+                if (abstractControl.value === '') {
+                    const fullPath = '/basket-delete' + path;
+                    localStorage.setItem(fullPath, abstractControl.value);
+                    console.log('Changed PATH: ' + fullPath + ' && Value = ' + abstractControl.value);
                 } else {
-                    const fullPath = path + '/' + key;
-                    console.log('Unchanged PATH: ' + fullPath + ' && Value = ' + abstractControl.value);
+                    const fullPath = '/basket-update' + path;
+                    localStorage.setItem(fullPath, abstractControl.value);
+                    console.log('Changed PATH: ' + fullPath + ' && Value = ' + abstractControl.value);
                 }
+            } else {
+                const fullPath = path;
+                console.log('Unchanged PATH: ' + fullPath + ' && Value = ' + abstractControl.value);
             }
-        });
+        }
     }
 }
