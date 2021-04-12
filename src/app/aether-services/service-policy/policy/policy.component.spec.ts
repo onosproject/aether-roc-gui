1d01e329571d860293280380287301e31bd8d2e7
jamesk1997 <39774519+jamesk1997@users.noreply.github.com>
2021-04-12 14:07:55 +0100

    Fixing linter (#67)

--- src/app/aether-services/service-policy/policy/policy.component.spec.ts
+++ src/app/aether-services/service-policy/policy/policy.component.spec.ts
@@ -1,25 +1,61 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/*
+ * SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+ *
+ * SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+ */
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PolicyComponent } from './policy.component';
+import {PolicyComponent} from './policy.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {ApiModule} from '../../../../openapi3/aether/2.1.0/api.module';
+import {MatTableModule} from '@angular/material/table';
 
 describe('PolicyComponent', () => {
-  let component: PolicyComponent;
-  let fixture: ComponentFixture<PolicyComponent>;
+    let component: PolicyComponent;
+    let fixture: ComponentFixture<PolicyComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PolicyComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [PolicyComponent],
+            imports: [
+                HttpClientTestingModule,
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                FormsModule,
+                ReactiveFormsModule,
+                MatInputModule,
+                MatFormFieldModule,
+                MatIconModule,
+                MatToolbarModule,
+                MatCardModule,
+                MatButtonModule,
+                MatDividerModule,
+                MatSnackBarModule,
+                MatTableModule,
+                ApiModule
+            ]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PolicyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PolicyComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
