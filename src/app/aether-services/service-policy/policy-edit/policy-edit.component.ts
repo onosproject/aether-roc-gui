2b2a63af85a8e567887a80bf45dc503a34dcef9e
hanielSceva95 <47032044+hanielSceva95@users.noreply.github.com>
2021-05-03 12:17:58 -0700

    Numberic values bandwidth (#76)
    * Added dropdown for required input fields
    
    * Disabling the update button if the input fields are not touched or valid
    
    * adding the dropdown for bandwidth for qos and service-policy
    
    * Cleared the lint errors
    
    * Cleared the lint errors 2

--- src/app/aether-services/service-policy/policy-edit/policy-edit.component.ts
+++ src/app/aether-services/service-policy/policy-edit/policy-edit.component.ts
@@ -12,6 +12,10 @@ import {BasketService, IDATTRIBS, TYPE} from '../../../basket.service';
 import {RocEditBase} from '../../../roc-edit-base';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {OpenPolicyAgentService} from '../../../open-policy-agent.service';
+import {Observable} from 'rxjs';
+import {Bandwidths} from '../../service-rule/rule-edit/rule-edit.component';
+import {map, startWith} from 'rxjs/operators';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 @Component({
     selector: 'aether-policy-edit',
@@ -22,6 +26,17 @@ import {OpenPolicyAgentService} from '../../../open-policy-agent.service';
 })
 export class PolicyEditComponent extends RocEditBase<ServicePolicyServicePolicy> implements OnInit {
     data: ServicePolicyServicePolicy;
+    options: Bandwidths[] = [
+        { megabyte : { numerical : 1048576, inMb: '1Mb'} },
+        { megabyte : { numerical : 2097152, inMb: '2Mb'} },
+        { megabyte : { numerical : 5242880, inMb: '5Mb'} },
+        { megabyte : { numerical : 1048576, inMb: '10Mb'} },
+        { megabyte : { numerical : 26214400, inMb: '25Mb'} },
+        { megabyte : { numerical : 52428800, inMb: '50Mb'} },
+        { megabyte : { numerical: 104857600, inMb: '100Mb'} },
+        { megabyte : { numerical: 524288000, inMb: '500Mb'}}
+    ];
+    bandwidthOptions: Observable<Bandwidths[]>;
     showAddComponent: boolean = false;
 
     policyForm = this.fb.group({
@@ -109,6 +124,17 @@ export class PolicyEditComponent extends RocEditBase<ServicePolicyServicePolicy>
 
     ngOnInit(): void {
         super.init();
+        this.bandwidthOptions = this.policyForm.valueChanges
+            .pipe(
+                startWith(''),
+                map(value => typeof value === 'number' ? value : value.megabyte),
+                map(megabyte => megabyte ? this._filter(megabyte) : this.options.slice())
+            );
+    }
+
+    private _filter(bandwidthIndex: number): Bandwidths[] {
+        const filterValue = bandwidthIndex;
+        return this.options.filter(option => option.megabyte.numerical);
     }
 
     loadServicePolicyServicePolicy(target: string, id: string): void {
--- src/app/aether-services/service-rule/rule-edit/rule-edit.component.ts
+++ src/app/aether-services/service-rule/rule-edit/rule-edit.component.ts
@@ -11,9 +11,14 @@ import {ServiceRuleServiceRule} from '../../../../openapi3/aether/2.1.0/models';
 import {BasketService, IDATTRIBS, TYPE} from '../../../basket.service';
 import {RocEditBase} from '../../../roc-edit-base';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {Observable} from 'rxjs';
 import {OpenPolicyAgentService} from '../../../open-policy-agent.service';
+import {map, startWith} from 'rxjs/operators';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
-
+export interface Bandwidths {
+    megabyte: { numerical: number, inMb: string};
+}
 @Component({
     selector: 'aether-rule-edit',
     templateUrl: './rule-edit.component.html',
@@ -23,6 +28,17 @@ import {OpenPolicyAgentService} from '../../../open-policy-agent.service';
 })
 export class RuleEditComponent extends RocEditBase<ServiceRuleServiceRule> implements OnInit {
     data: ServiceRuleServiceRule;
+    options: Bandwidths[] = [
+        { megabyte : { numerical : 1048576, inMb: '1Mb'} },
+        { megabyte : { numerical : 2097152, inMb: '2Mb'} },
+        { megabyte : { numerical : 5242880, inMb: '5Mb'} },
+        { megabyte : { numerical : 1048576, inMb: '10Mb'} },
+        { megabyte : { numerical : 26214400, inMb: '25Mb'} },
+        { megabyte : { numerical : 52428800, inMb: '50Mb'} },
+        { megabyte : { numerical: 104857600, inMb: '100Mb'} },
+        { megabyte : { numerical: 524288000, inMb: '500Mb'}}
+    ];
+    bandwidthOptions: Observable<Bandwidths[]>;
     ruleForm = this.fb.group({
         id: ['', Validators.compose([
             Validators.minLength(1),
@@ -100,6 +116,12 @@ export class RuleEditComponent extends RocEditBase<ServiceRuleServiceRule> imple
 
     ngOnInit(): void {
         super.init();
+        this.bandwidthOptions = this.ruleForm.valueChanges
+            .pipe(
+                startWith(''),
+                map(value => typeof value === 'number' ? value : value.megabyte),
+                map(megabyte => megabyte ? this._filter(megabyte) : this.options.slice())
+            );
     }
 
     get ambControls(): FormArray {
@@ -108,6 +130,10 @@ export class RuleEditComponent extends RocEditBase<ServiceRuleServiceRule> imple
 
     get gbControls(): FormArray {
         return this.ruleForm.get(['qos', 'guaranteed-bitrate']) as FormArray;
+    }
+    private _filter(bandwidthIndex: number): Bandwidths[] {
+        const filterValue = bandwidthIndex;
+        return this.options.filter(option => option.megabyte.numerical);
     }
 
     loadServiceRuleServiceRule(target: string, id: string): void {
