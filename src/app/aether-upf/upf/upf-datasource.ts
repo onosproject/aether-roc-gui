8bd529e4bd0adb189a4779ee49b893758dc159b7
haniel sceva <hmanda1@binghamton.edu>
2021-06-10 11:57:39 -0700

    UPF module added

---
+++ src/app/aether-upf/upf/upf-datasource.ts
@@ -0,0 +1,22 @@
+/*
+ * SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+ *
+ * SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+ */
+
+import {Upf, UpfUpf} from '../../../openapi3/aether/3.0.0/models';
+import {Service as AetherService} from '../../../openapi3/aether/3.0.0/services';
+import {BasketService} from '../../basket.service';
+import {RocDataSource} from '../../roc-data-source';
+
+export class UpfDatasource extends RocDataSource<UpfUpf, Upf> {
+
+    constructor(
+        protected aetherService: AetherService,
+        protected bs: BasketService,
+        protected target: string,
+    ) {
+        super( aetherService, bs, target,
+            '/upf-3.0.0', 'upf' );
+    }
+}
