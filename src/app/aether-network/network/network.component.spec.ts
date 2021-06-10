9138d1470be84455d801ccb06f07f62d7fdfaac1
hanielSceva95 <47032044+hanielSceva95@users.noreply.github.com>
2021-06-10 02:10:51 -0700

    Network Module's list page added (#90)
    * Network Module list page has been added
    
    * Cleared lint errors
    
    * Changing the header for MCC

---
+++ src/app/aether-network/network/network.component.spec.ts
@@ -0,0 +1,56 @@
+/*
+ * SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+ *
+ * SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+ */
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApiModule } from 'src/openapi3/aether/3.0.0/api.module';
+import {of} from 'rxjs';
+import { NetworkComponent } from './network.component';
+
+describe('NetworkComponent', () => {
+  let component: NetworkComponent;
+  let fixture: ComponentFixture<NetworkComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NetworkComponent ],
+      imports: [
+          HttpClientTestingModule,
+          RouterTestingModule,
+          BrowserAnimationsModule,
+          MatPaginatorModule,
+          MatSortModule,
+          MatTableModule,
+          MatSnackBarModule,
+          MatToolbarModule,
+          MatIconModule,
+          ApiModule
+      ],
+      providers: [
+          {provide: ActivatedRoute, useValue: {paramMap: of({ get: (key) => 'value' })}},
+      ],
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NetworkComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
