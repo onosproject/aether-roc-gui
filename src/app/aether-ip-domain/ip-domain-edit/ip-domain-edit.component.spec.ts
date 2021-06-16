734690f3e7df81df2e31fcc5d6227f4bdf37b694
jamesk1997 <39774519+jamesk1997@users.noreply.github.com>
2021-06-16 13:40:52 +0100

    Implmented edit page for IP Domain (#103)

---
+++ src/app/aether-ip-domain/ip-domain-edit/ip-domain-edit.component.spec.ts
@@ -0,0 +1,59 @@
+/*
+ * SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+ *
+ * SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+ */
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {IpDomainEditComponent} from './ip-domain-edit.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
+
+describe('IpDomainEditComponent', () => {
+    let component: IpDomainEditComponent;
+    let fixture: ComponentFixture<IpDomainEditComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [IpDomainEditComponent],
+            imports: [
+                HttpClientTestingModule,
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                FormsModule,
+                ReactiveFormsModule,
+                MatInputModule,
+                MatFormFieldModule,
+                MatSelectModule,
+                MatIconModule,
+                MatToolbarModule,
+                MatCardModule,
+                MatButtonModule,
+                MatSnackBarModule,
+                MatSlideToggleModule
+            ]
+        })
+            .compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(IpDomainEditComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
