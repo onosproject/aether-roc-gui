af7da21753ef36d085e235d4cde4146a7855ce6e
jamesk1997 <39774519+jamesk1997@users.noreply.github.com>
2021-06-29 12:19:44 +0100

    Implementing Device Group Edit Page (#119)

---
+++ src/app/aether-device-group/device-group-edit/device-group-edit.component.spec.ts
@@ -0,0 +1,60 @@
+/*
+ * SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+ *
+ * SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+ */
+
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {DeviceGroupEditComponent} from './device-group-edit.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+describe('DeviceGroupEditComponent', () => {
+    let component: DeviceGroupEditComponent;
+    let fixture: ComponentFixture<DeviceGroupEditComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DeviceGroupEditComponent],
+            imports: [
+                HttpClientTestingModule,
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                FormsModule,
+                ReactiveFormsModule,
+                MatInputModule,
+                MatFormFieldModule,
+                MatIconModule,
+                MatToolbarModule,
+                MatCardModule,
+                MatButtonModule,
+                MatDividerModule,
+                MatSnackBarModule,
+                MatSelectModule
+            ]
+        })
+            .compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DeviceGroupEditComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/app/aether-device-group/imsis-select/imsis-select.component.spec.ts
@@ -0,0 +1,59 @@
+/*
+ * SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+ *
+ * SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+ */
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ImsisSelectComponent} from './imsis-select.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+describe('ImsisSelectComponent', () => {
+    let component: ImsisSelectComponent;
+    let fixture: ComponentFixture<ImsisSelectComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ImsisSelectComponent],
+            imports: [
+                HttpClientTestingModule,
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                FormsModule,
+                ReactiveFormsModule,
+                MatInputModule,
+                MatFormFieldModule,
+                MatIconModule,
+                MatToolbarModule,
+                MatCardModule,
+                MatButtonModule,
+                MatDividerModule,
+                MatSnackBarModule,
+                MatSelectModule
+            ]
+        })
+            .compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ImsisSelectComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
