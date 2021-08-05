483b492a13ef9932b7f9042f988463e913032f0b
Sean Condon <sean@opennetworking.org>
2021-08-05 13:23:50 +0100

    adding a details panel for alerts

---
+++ src/app/dashboard/alert-detail/alert-detail.component.spec.ts
@@ -0,0 +1,42 @@
+/*
+ * SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+ *
+ * SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+ */
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {AlertDetailComponent} from './alert-detail.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
+describe('AlertDetailComponent', () => {
+    let component: AlertDetailComponent;
+    let fixture: ComponentFixture<AlertDetailComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AlertDetailComponent],
+            imports: [
+                HttpClientTestingModule,
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                MatToolbarModule,
+                MatCardModule,
+            ],
+        })
+            .compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AlertDetailComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
