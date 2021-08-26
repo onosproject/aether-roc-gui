3d510f02f9244d655a5038a50cdad5026989ae9d
haniel sceva <hmanda1@binghamton.edu>
2021-08-26 09:37:45 -0700

    Changing the IMSI range validation in device-group to allow one UE per IMSI range

--- src/app/aether-device-group/device-group-edit/device-group-edit.component.ts
+++ src/app/aether-device-group/device-group-edit/device-group-edit.component.ts
@@ -35,7 +35,7 @@ const ValidateImsiRange: ValidatorFn = (control: AbstractControl): ValidationErr
                 if (eachImsiFormValues.name !== eachImsi.name) {
                     isValid = ((eachImsi['imsi-range-to'] < eachImsiFormValues['imsi-range-from'] ||
                         eachImsi['imsi-range-from'] > eachImsiFormValues['imsi-range-to'])
-                        && (eachImsi['imsi-range-from'] < eachImsi['imsi-range-to'] &&
+                        && (eachImsi['imsi-range-from'] <= eachImsi['imsi-range-to'] &&
                             eachImsi['imsi-range-to'] <= (100 + (eachImsi['imsi-range-from'])))) ? null : {isRangeNotValid: true};
                 }
             }
