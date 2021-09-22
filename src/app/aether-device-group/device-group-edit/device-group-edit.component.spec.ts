91bdf4191ff956934319c57f046a46e85353a2e5
Sean Condon <sean@opennetworking.org>
2021-09-22 18:41:04 +0100

    AETHER-2291 increase imsi-range size to 5000 (from 100)

--- src/app/aether-device-group/device-group-edit/device-group-edit.component.spec.ts
+++ src/app/aether-device-group/device-group-edit/device-group-edit.component.spec.ts
@@ -13,23 +13,26 @@ import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/mater
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormArray, FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {RouterTestingModule} from '@angular/router/testing';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 describe('DeviceGroupEditComponent', () => {
     let component: DeviceGroupEditComponent;
     let fixture: ComponentFixture<DeviceGroupEditComponent>;
+    const fb = new FormBuilder();
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [DeviceGroupEditComponent],
             providers: [
-                {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'standard'}}
+                {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'standard'}},
+                {provide: FormBuilder, useValue: fb}
             ],
             imports: [
                 HttpClientTestingModule,
@@ -45,7 +48,8 @@ describe('DeviceGroupEditComponent', () => {
                 MatButtonModule,
                 MatDividerModule,
                 MatSnackBarModule,
-                MatSelectModule
+                MatSelectModule,
+                MatTooltipModule
             ]
         })
             .compileComponents();
@@ -55,9 +59,145 @@ describe('DeviceGroupEditComponent', () => {
         fixture = TestBed.createComponent(DeviceGroupEditComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        component.deviceGroupForm.get('id').setValue('testDg');
+        component.deviceGroupForm.get('description').setValue('testDg');
+        component.deviceGroupForm.get('display-name').setValue('testDg');
+        component.deviceGroupForm.get('ip-domain').setValue('testDg');
+        component.deviceGroupForm.get('site').setValue('testDg');
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it ('should validate a single range', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(10),
+            'imsi-range-to': fb.control(19),
+        }));
+        expect(component.deviceGroupForm.valid).toBeTruthy();
+    });
+
+    it ('should validate an equal range', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(10),
+            'imsi-range-to': fb.control(10),
+        }));
+        expect(component.deviceGroupForm.valid).toBeTruthy();
+    });
+
+    it ('should validate a max range', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(10),
+            'imsi-range-to': fb.control(5010),
+        }));
+        expect(component.deviceGroupForm.valid).toBeTruthy();
+    });
+
+    it ('should not validate an excessive range in a single', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(10),
+            'imsi-range-to': fb.control(5011),
+        }));
+        expect(component.deviceGroupForm.valid).toBeFalse();
+    });
+
+    it ('should not validate an excessive range in a double', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(10),
+            'imsi-range-to': fb.control(15),
+        }));
+        imsisControlArray.push(fb.group({
+            name: fb.control("second"),
+            'imsi-range-from': fb.control(20),
+            'imsi-range-to': fb.control(5021),
+        }));
+        expect(component.deviceGroupForm.valid).toBeFalse();
+    });
+
+    it ('should validate a double range', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(10),
+            'imsi-range-to': fb.control(19),
+        }));
+        imsisControlArray.push(fb.group({
+            name: fb.control("second"),
+            'imsi-range-from': fb.control(20),
+            'imsi-range-to': fb.control(29),
+        }));
+        expect(component.deviceGroupForm.valid).toBeTruthy();
+    });
+
+    it ('should validate a juxtaposed triple range', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(300),
+            'imsi-range-to': fb.control(400),
+        }));
+        imsisControlArray.push(fb.group({
+            name: fb.control("second"),
+            'imsi-range-from': fb.control(10),
+            'imsi-range-to': fb.control(20),
+        }));
+        imsisControlArray.push(fb.group({
+            name: fb.control("second"),
+            'imsi-range-from': fb.control(0),
+            'imsi-range-to': fb.control(1),
+        }));
+        expect(component.deviceGroupForm.valid).toBeTruthy();
+    });
+
+    it ('should not validate a single reversed range', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(11),
+            'imsi-range-to': fb.control(10),
+        }));
+        expect(component.deviceGroupForm.valid).toBeFalse();
+    });
+
+    it ('should not validate a reversed range in a double', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(11),
+            'imsi-range-to': fb.control(10),
+        }));
+        imsisControlArray.push(fb.group({
+            name: fb.control("second"),
+            'imsi-range-from': fb.control(20),
+            'imsi-range-to': fb.control(29),
+        }));
+        expect(component.deviceGroupForm.valid).toBeFalse();
+    });
+
+    it ('should not validate an overlapping range', () => {
+        const imsisControlArray = component.deviceGroupForm.get('imsis') as FormArray;
+        imsisControlArray.push(fb.group({
+            name: fb.control("first"),
+            'imsi-range-from': fb.control(10),
+            'imsi-range-to': fb.control(19),
+        }));
+        imsisControlArray.push(fb.group({
+            name: fb.control("second"),
+            'imsi-range-from': fb.control(15),
+            'imsi-range-to': fb.control(25),
+        }));
+        expect(component.deviceGroupForm.valid).toBeFalse();
+    });
+
 });
