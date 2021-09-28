2dcec1b6b6b3370f42f261c18ad50524bd028e6a
prateeksarda <34772536+prateeksarda@users.noreply.github.com>
2021-09-28 21:02:00 +0530

    Added Site panel for dashboard  (#177)
    * Added Site panel for dashboard Fixes https://jira.opennetworking.org/browse/AETHER-2156
    
    * removed was only for test
    
    * updating path attribute

---
+++ src/app/utils/site-prom-data-source.ts
@@ -0,0 +1,56 @@
+/*
+ * SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+ *
+ * SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+ */
+import {from, Observable} from 'rxjs';
+import {PROMETHEUS_PROXY} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {mergeMap, pluck} from 'rxjs/operators';
+
+export interface SiteSiteProm {
+    id: string;
+    health: string;
+}
+
+export interface ResultMetric {
+    __name__: string;
+    instance: string;
+    job: string;
+    name: string;
+}
+
+export interface ResultItem {
+    metric: ResultMetric;
+    value: any[];
+}
+
+export interface PromResultData {
+    resultType: string;
+    result: ResultItem[];
+}
+
+export interface PromResult {
+    status: string;
+    data: PromResultData;
+}
+
+export class SitePromDataSource {
+
+    constructor(
+        private httpClient: HttpClient,
+    ) {
+    }
+
+    loadData(tags: string[]): Observable<ResultItem> {
+        const url = this.queryBuilder(tags);
+        return this.httpClient.get<PromResultData>(url).pipe(
+            pluck('data'),
+            mergeMap((items: PromResultData) => from(items.result)),
+        );
+    }
+
+    queryBuilder(tags: string[]): string {
+        return PROMETHEUS_PROXY + '/api/v1/query?query={__name__=~"' + tags.join('|') + '"}';
+    }
+}
