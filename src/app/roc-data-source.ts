f3d388748489219aa4466b9048f96cef5ddc1204
haniel sceva <hmanda1@binghamton.edu>
2021-11-17 10:11:18 -0800

    removing the unwanted code

--- src/app/roc-data-source.ts
+++ src/app/roc-data-source.ts
@@ -98,10 +98,6 @@ export abstract class RocDataSource<T, U> extends DataSource<T> {
         ).subscribe(
             (value => {
                 const id = value[this.indexAttr];
-                // console.log(this.pathListAttr, "this.pathRoot")
-                // if (this.pathRoot !== "/vcs-4.0.0" && this.pathListAttr !== this.pathListAttr) {
-                //
-                // }
                 if (!this.bs.containsDeleteEntry(
                     this.pathRoot + '/' + this.pathListAttr + '[' + this.indexAttr + '=' + id + ']/' + this.indexAttr) || (this.pathRoot === "/vcs-4.0.0" && this.pathListAttr === this.pathListAttr)) {
                     this.data.push(value);
