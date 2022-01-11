dd7f996eef2ff13a0ed5795b9471f021b2052672
Sean Condon <sean@opennetworking.org>
2022-01-11 18:15:13 +0000

    Add in coverage limits and tighter linting (#262)

--- karma.conf.js
+++ karma.conf.js
@@ -19,16 +19,27 @@ module.exports = function (config) {
             require("karma-jasmine"),
             require("karma-chrome-launcher"),
             require("karma-jasmine-html-reporter"),
-            require("karma-coverage-istanbul-reporter"),
+            require("karma-coverage"),
             require("@angular-devkit/build-angular/plugins/karma"),
         ],
         client: {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
+        coverageReporter: {
             dir: require("path").join(__dirname, "./coverage/aether-roc-gui"),
-            reports: ["html", "lcovonly", "text-summary"],
-            fixWebpackSourcePaths: true,
+            subdir: ".",
+            reporters: [
+                { type: "html" },
+                { type: "text-summary" }
+            ],
+            check: {
+                global: {
+                    statements: 34, // TODO: increase all to 80%
+                    branches: 12,
+                    functions: 22,
+                    lines: 32
+                }
+            }
         },
         reporters: ["progress", "kjhtml"],
         port: 9876,
