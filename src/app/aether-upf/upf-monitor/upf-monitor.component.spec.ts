e83dcb08d9008af90d03ff03249b89396fe98a0b
Prateek Sarda <prateek.sarda@opennetworking.org>
2022-02-07 17:14:27 +0530

    Adding UPF Monitor page and links

---
+++ src/app/aether-upf/upf-monitor/upf-monitor.component.spec.ts
@@ -0,0 +1,78 @@
+/*
+ * SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UpfMonitorComponent } from './upf-monitor.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { GRAFANA_PROXY } from '../../../environments/environment';
+import { UtilsModule } from '../../utils/utils.module';
+import {
+    OAuthLogger,
+    OAuthModule,
+    OAuthService,
+    UrlHelperService,
+} from 'angular-oauth2-oidc';
+
+describe('SiteMonitorComponent', () => {
+    let component: UpfMonitorComponent;
+    let fixture: ComponentFixture<UpfMonitorComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UpfMonitorComponent],
+            imports: [
+                HttpClientTestingModule,
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                FormsModule,
+                ReactiveFormsModule,
+                MatInputModule,
+                MatFormFieldModule,
+                MatIconModule,
+                MatToolbarModule,
+                MatCardModule,
+                MatButtonModule,
+                MatDividerModule,
+                MatSnackBarModule,
+                MatSlideToggleModule,
+                MatAutocompleteModule,
+                MatSelectModule,
+                UtilsModule,
+                OAuthModule.forRoot(),
+            ],
+            providers: [
+                { provide: OAuthService },
+                { provide: UrlHelperService },
+                { provide: OAuthLogger },
+                { provide: 'grafana_api_proxy', useValue: GRAFANA_PROXY },
+            ],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UpfMonitorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
