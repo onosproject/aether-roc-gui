f4b9b2ace3908fb8a34413d00c4a28dd0f9e2b88
Sean Condon <sean.condon@intel.com>
2022-04-14 12:05:56 +0000

    Aether-3395 model updates for Aether 2.1.0 models (#319)

--- src/openapi3/top/level/models/elements.ts
+++ src/openapi3/top/level/models/elements.ts
@@ -5,6 +5,12 @@ import {
     ConnectivityServices as ConnectivityServices200,
     Enterprises as Enterprises200
 } from "../../../aether/2.0.0/models";
+import {
+    Application as Application210,
+    Site as Site210,
+    TrafficClass as TrafficClass210,
+    Template as Template210
+} from "../../../aether/2.1.0/models";
 
 export interface Elements {
 
@@ -12,9 +18,29 @@ export interface Elements {
      * The connectivity-services top level container
      */
     'connectivity-services-2.0.0'?: ConnectivityServices200;
-    
+
     /**
      * The top level enterprises container
      */
     'enterprises-2.0.0'?: Enterprises200;
+
+    /**
+     * The top level application container
+     */
+    'application-2.1.0'?: Application210
+
+    /**
+     * The top level application container
+     */
+    'site-2.1.0'?: Site210
+
+    /**
+     * The top level application container
+     */
+    'traffic-class-2.1.0'?: TrafficClass210
+
+    /**
+     * The top level application container
+     */
+    'template-2.1.0'?: Template210
 }
